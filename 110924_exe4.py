2e320e986b80984053efdde5d2eb949068d08b37
Joao Vitor Brustolin Oliveira <jvoliveira160916@gmail.com>
2024-09-11 14:47:58 -0300

    senai
    senai

---
+++ 110924_exe4.py
@@ -0,0 +1,17 @@
+def pode_se_alistar(idade, sexo):
+
+    if idade >= 18:
+        
+        if sexo.lower() == 'm':
+            return "Pode se alistar."
+        else:
+            return "Não pode se alistar."
+    else:
+        return "Não pode se alistar."
+
+
+idade = int(input("Digite a idade da pessoa: "))
+sexo = input("Digite o sexo da pessoa (m/f): ").strip().lower()
+
+resultado = pode_se_alistar(idade, sexo)
+print(resultado)
